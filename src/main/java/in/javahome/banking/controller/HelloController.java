1755a85004337a99ab8c3e48d8698fe37ca805f2
javahometech <hari.kammana@gmail.com>
2023-08-30 09:47:28 +0530

    Update HelloController.java

--- src/main/java/in/javahome/banking/controller/HelloController.java
+++ src/main/java/in/javahome/banking/controller/HelloController.java
@@ -3,6 +3,8 @@ package in.javahome.banking.controller;
 public class HelloController {
 	String name = "Java Home";
 	public void add(int x, int y) {
+		String x = null;
+		x.toString();
 		int result = x+y;
 		System.out.println("result is"+result);
 	}
